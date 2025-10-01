b8d8b7b18bf7f4db3f2294e5ed298194e359eb81
Rosie Nasta <rnasta3@pride.hofstra.edu>
2025-10-01 15:00:47 -0400

    Added env variables

--- api/login/firebase.js
+++ api/login/firebase.js
@@ -1,23 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBa5cJd9ohrN9Cp4TWvw78nJx5wEAZyFno",
-  authDomain: "re-hardware-store.firebaseapp.com",
-  projectId: "re-hardware-store",
-  storageBucket: "re-hardware-store.firebasestorage.app",
-  messagingSenderId: "527829643677",
-  appId: "1:527829643677:web:e4a0269f413df08f14ff74",
-  measurementId: "G-6NDP6F14HN"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 export { auth };
+export const signInWithGoogle = () => {
+  return signInWithPopup(auth, provider);
+}
