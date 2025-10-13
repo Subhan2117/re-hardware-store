752804de3ba02dbc3a0bc9859a2f1198638cb968
Subhan2117 <Subhannadeem06@gmail.com>
2025-10-13 12:15:46 -0400

    fixed partial rendering and placed the admin side bar at the root layout to prevent sidebar rendering on every page route improved performace and efficiency

--- app/(admin)/admin/products/page.jsx
+++ app/(admin)/admin/products/page.jsx
@@ -1,3 +1,7 @@
-export default function page(){
-    
+export default function page() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-orange-50 via-amber-50 to-white">
+      <div className="">Products</div>
+    </div>
+  );
 }
